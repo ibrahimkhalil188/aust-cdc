a5760e2534757d00ddd7f6d4774170f1f9f1634f
unknown <ibrahimkhalil.mine@gmail.com>
2022-11-14 21:21:27 +0600

    add slider

--- src/Components/Home/slider.js
+++ src/Components/Home/slider.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -9,21 +9,27 @@ import "swiper/css/pagination";
 import "../../Style/slider.style.css";
 
 // import required modules
-import { Pagination } from "swiper";
-
+import { Autoplay, Navigation, Pagination } from "swiper";
+import image1 from "../../Assets/Images/austBg.png";
+import image2 from "../../Assets/Images/eventBG-01.png";
 const Slider = ()=> {
   return (
     <>
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper swiper swiper-slice">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+      <Swiper spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper">
+        <SwiperSlide><img src={image1} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={image2} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={image1} alt=""/></SwiperSlide>
       </Swiper>
     </>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import AboutAust from '../Components/Home/AboutAust';
 import AboutWithLogo from '../Components/Home/AboutWithLogo';
 import Affiliation from '../Components/Home/Affiliation';
 import AustCdcPillers from '../Components/Home/AustCdcPillers';
 import EventWithFixedBg from '../Components/Home/EventWithFixedBg';
-import HeroSection from '../Components/Home/HeroSection';
 import LatestNews from '../Components/Home/LatestNews';
-import AboutAust from '../Components/Home/AboutAust';
+import Slider from '../Components/Home/slider';
 
 const Home = () => {
     return (
         <div>
-            <HeroSection></HeroSection>
+            {/* <HeroSection></HeroSection> */}
+            <Slider></Slider>
             <AustCdcPillers></AustCdcPillers>
             <AboutWithLogo></AboutWithLogo>
             <AboutAust></AboutAust>
