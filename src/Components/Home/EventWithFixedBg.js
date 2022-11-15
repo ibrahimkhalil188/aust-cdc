3a03401c2633642527e66ac10dbb15f61d8e45c1
unknown <ibrahimkhalil.mine@gmail.com>
2022-11-15 19:26:46 +0600

    event and latest news

--- src/Components/Home/EventWithFixedBg.js
+++ src/Components/Home/EventWithFixedBg.js
@@ -11,13 +11,34 @@ import "../../Style/slider.style.css";
 
 // import required modules
 import { Autoplay, Navigation, Pagination } from "swiper";
-
+import image1 from "../../Assets/Images/aust_logo.svg";
 const EventWithFixedBg = () => {
+  const event = [
+    {
+      id:"1",
+      title:"Event tittle",
+      image:image1,
+      des:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi voluptatibus unde necessitatibus quae, molestiae ea consequatur iste iusto nihil, odit quaerat enim officia nam voluptas obcaecati! Quod, ad libero?"
+    },
+    {
+      id:"2",
+      title:"Event tittle",
+      image:image1,
+      des:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi voluptatibus unde necessitatibus quae, molestiae ea consequatur iste iusto nihil, odit quaerat enim officia nam voluptas obcaecati! Quod, ad libero?"
+    },
+    {
+      id:"3",
+      image:image1,     
+      title:"Event tittle",
+      des:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi voluptatibus unde necessitatibus quae, molestiae ea consequatur iste iusto nihil, odit quaerat enim officia nam voluptas obcaecati! Quod, ad libero?"
+    }
+  ]
   return (
+   
     <div>
      < div className=" flex items-center flex-col">
-          <h1 className="text-6xl mt-6 text-secondary">EVENTS</h1>
-          <div className="ml-[10px]"><div className="bg-primary h-1 w-[210px]"></div></div>
+          <h1 className="text-6xl mt-16 text-secondary">EVENTS</h1>
+          <div className="ml-[10px] mb-12"><div className="bg-primary h-1 w-[210px]"></div></div>
          {/*  <div className=" h-1 w-48 bg-white"></div> */}
         </div>
         <Swiper
@@ -39,68 +60,31 @@ const EventWithFixedBg = () => {
         }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper h-[85vh] relative"
+        className="mySwiper"
       >
-        <SwiperSlide>
-        <div className="hero min-h-screen">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="https://placeimg.com/260/400/arch" alt=""
-              className=" w-[550px] h-[450px] rounded-lg shadow-2xl"
-            />
-            <div className="text-white ">
-              <h1 className="text-6xl mx-16 font-bold border-b-4 border-secondary mt-[-200px] uppercase">
-                title of the event
-              </h1>
-              <p className="py-6 mx-16">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
+        {
+          event.map(singleEvent=><SwiperSlide>
+            <div className="hero min-h-screen">
+              <div className="hero-content flex-col lg:flex-row">
+                <img style={{width:"450px"}}
+                  src={singleEvent.image} alt=""
+                />
+                <div className="text-white ">
+                  <div>
+                  <h1 className="text-6xl mx-16 font-bold border-secondary mt-[-200px] uppercase text-start">
+                   {singleEvent.title}
+                  </h1>
+                  <div className="bg-secondary ml-16 h-1 w-[370px]"></div>
+                  </div>
+                  <p className="py-6 mx-16 text-justify">
+                    {singleEvent.des}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        </SwiperSlide>
-        <SwiperSlide>
-        <div className="hero min-h-screen">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="https://placeimg.com/260/400/arch" alt=""
-              className=" w-[550px] h-[450px] rounded-lg shadow-2xl"
-            />
-            <div className="text-white ">
-              <h1 className="text-6xl mx-16 font-bold border-b-4 border-secondary mt-[-200px] uppercase">
-                title of the event
-              </h1>
-              <p className="py-6 mx-16">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-            </div>
-          </div>
-        </div>
-        </SwiperSlide>
-        <SwiperSlide>
-        <div className="hero min-h-screen">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="https://placeimg.com/260/400/arch" alt=""
-              className=" w-[550px] h-[450px] rounded-lg shadow-2xl"
-            />
-            <div className="text-white ">
-              <h1 className="text-6xl mx-16 font-bold border-b-4 border-secondary mt-[-200px] uppercase">
-                title of the event
-              </h1>
-              <p className="py-6 mx-16">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-            </div>
-          </div>
-        </div>
-        </SwiperSlide>
+            </SwiperSlide>)
+        }
+        
       </Swiper>
       </div>
   );
--- src/Components/Home/LatestNews.js
+++ src/Components/Home/LatestNews.js
@@ -1,12 +1,87 @@
-import React from 'react';
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "../../Style/slider.style.css";
+
+// import required modules
+import { Autoplay, Navigation, Pagination } from "swiper";
+import image1 from "../../Assets/Images/aust_logo.svg";
 const LatestNews = () => {
-    return (
-        <div className='h-[80vh] w-full shadow-lg'>
-            <h1 className='text-4xl text-center'>Latest News</h1>
-            
+  const news = [
+    {
+      id:"1",
+      title:"Event tittle",
+      image:image1,
+      des:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi voluptatibus unde necessitatibus quae, molestiae ea consequatur iste iusto nihil, odit quaerat enim officia nam voluptas obcaecati! Quod, ad libero?"
+    },
+    {
+      id:"2",
+      title:"Event tittle",
+      image:image1,
+      des:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi voluptatibus unde necessitatibus quae, molestiae ea consequatur iste iusto nihil, odit quaerat enim officia nam voluptas obcaecati! Quod, ad libero?"
+    },
+    {
+      id:"3",
+      image:image1,     
+      title:"Event tittle",
+      des:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi voluptatibus unde necessitatibus quae, molestiae ea consequatur iste iusto nihil, odit quaerat enim officia nam voluptas obcaecati! Quod, ad libero?"
+    }
+  ]
+  return (
+   
+    <div>
+     < div className=" flex items-center flex-col">
+          <h1 className="text-6xl mt-16 text-primary uppercase">Latest News</h1>
+          <div className="ml-[10px]"><div className="bg-secondary h-1 w-[350px]"></div></div>
+         {/*  <div className=" h-1 w-48 bg-white"></div> */}
         </div>
-    );
+        <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {
+          news.map(singleNews=><SwiperSlide>
+            <div className="hero min-h-screen">
+              <div className="hero-content flex-col lg:flex-row">
+                
+                <div>
+                  <div>
+                  <h1 className="text-6xl mx-16 font-bold border-secondary mt-[-200px] uppercase text-start">
+                   {singleNews.title}
+                  </h1>
+                  <div className="bg-secondary ml-16 h-1 w-[370px]"></div>
+                  </div>
+                  <p className="py-6 mx-16 text-justify">
+                    {singleNews.des}
+                  </p>
+                </div>
+                <img style={{width:"450px"}}
+                  src={singleNews.image} alt=""
+                />
+              </div>
+            </div>
+            </SwiperSlide>)
+        }
+        
+      </Swiper>
+      </div>
+  );
 };
+
 
 export default LatestNews;
