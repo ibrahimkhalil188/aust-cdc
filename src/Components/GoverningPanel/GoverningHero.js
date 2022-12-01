8c8a0ded65fdff88b6b7769f2aecfae26e1fbdfa
unknown <ibrahimkhalil.mine@gmail.com>
2022-12-01 13:40:58 +0600

    govering hero responsive

--- src/Components/GoverningPanel/GoverningHero.js
+++ src/Components/GoverningPanel/GoverningHero.js
@@ -1,20 +1,34 @@
-import React from 'react';
-const GoverningHero = ({bannerData}) => {
-    return (
+import React from "react";
+const GoverningHero = ({ bannerData }) => {
+  return (
+    <div className="lg:h-[100vh] h-[50vh]">
+      <div
+        className="
+        bg-no-repeat 
+        bg-center 
+        bg-cover"
+        style={{
+          backgroundImage: `url(${bannerData.image})`,
+        }}
+      >
+        <div className="flex justify-center items-start  py-12  text-center lg:py-28 lg:pb-72 h-full">
         <div>
-            <div className="hero min-h-screen" style={{ backgroundImage: `url(${bannerData.image})`,backgroundPosition: 'center',backgroundSize: '100% 100%',
-    backgroundRepeat: 'no-repeat', height:"100vh" }}>
-  <div className=""></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div>
-    <h1 className="mt-[-230px] mb-8 text-8xl font-bold  uppercase"><span className="text-white">{bannerData.title.slice(0,-5)}</span> <span className="text-red-600">{bannerData.title.slice(-4)}</span></h1>
-      <button className=" hover:bg-red-600 bg-red-600 hover:border-red-600 border-red-600 text-4xl px-12 py-2 rounded-lg text-white uppercase font-sans font-bold">{bannerData.name}</button>
-     
-    </div>
-  </div>
-</div>
+            <h1 className="mb-8 text-4xl lg:text-8xl font-bold  uppercase">
+              <span className="text-white">
+                {bannerData.title.slice(0, -5)}
+              </span>{" "}
+              <span className="text-red-600">{bannerData.title.slice(-4)}</span>
+            </h1>
+            <div className="flex justify-center">
+            <button className=" hover:bg-red-600 bg-red-600 hover:border-red-600 border-red-600 text-2xl lg:text-4xl px-12 py-2 rounded-lg text-white uppercase font-sans font-bold">
+              {bannerData.name}
+            </button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default GoverningHero;
